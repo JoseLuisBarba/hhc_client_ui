304a0371cc2d230ac6d5ea951e2c742f20f8d953
José Luis Barba Farro <123603839+JoseLuisBarba@users.noreply.github.com>
2024-02-01 18:44:19 -0500

    finish login and create user

--- src/app/dashboard/pages/login/login.component.ts
+++ src/app/dashboard/pages/login/login.component.ts
@@ -1,14 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { HeaderComponent } from '@shared/header/header.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    HeaderComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
@@ -17,10 +22,30 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 export default class LoginComponent {
   
   imageUrl!:SafeUrl; 
+  imageBannerUrl!:SafeUrl;
+  loginForm = new FormGroup(
+    {
+      email: new FormControl('', [ Validators.required, Validators.email]),
+      password: new FormControl('', [ Validators.required, Validators.minLength(8)]),
 
-  constructor(private sanitizer: DomSanitizer) {
-    const imgRoute = 'assets/images/login/login.svg';
+    }
+  );
+  
+
+  constructor(
+    private sanitizer: DomSanitizer,
+  ) {
+    const imgRoute = 'assets/images/login/makedecisions_login.jpg';
+    const imgBannerRoute = 'assets/images/login/login.svg';
     this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(imgRoute);
+    this.imageBannerUrl = this.sanitizer.bypassSecurityTrustUrl(imgBannerRoute);
   }
 
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    console.log(this.loginForm.value);
+  }
 }
